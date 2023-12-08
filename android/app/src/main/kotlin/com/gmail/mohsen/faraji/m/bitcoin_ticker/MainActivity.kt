64b92a7467d7dc3170d87806842aac02f9338c82
mohsen0dev <mohsen.faraji.dev@gmail.com>
2023-12-08 15:30:59 +0330

    end

---
+++ android/app/src/main/kotlin/com/gmail/mohsen/faraji/m/bitcoin_ticker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gmail.mohsen.faraji.m.bitcoin_ticker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/coin_data.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const apiKey = '5BF2C951-7881-43A2-B5F0-63F2D4E9A5C6';
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
+
+      debugPrint('test1 $requestURL');
+      debugPrint(crypto);
+      http.Response response = await http.get(Uri.parse(requestURL));
+
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double price = decodedData['rate'];
+        cryptoPrices[crypto] = price.toStringAsFixed(0);
+        debugPrint('ok $cryptoPrices[crypto]');
+      } else {
+        debugPrint('no  ${response.statusCode}');
+        throw 'Problem with the get request';
+      }
+    }
+    return cryptoPrices;
+  }
+}
